4b5ca4cc14a35f4576ad9eb6c623f079998e76b7
Bounif Lynda <lynda.bounif@imadrassa.com>
2020-09-25 12:33:53 +0100

    add js scripts

---
+++ js/basics.js
@@ -0,0 +1,59 @@
+// 1. Simple affichage
+// console.log("Hello world")
+
+// 2. Les variables
+// let mot = "hello world !!!!@@@@@@"
+// let nombre = 7
+// let boolean = true
+// let boolean2 = 0
+// console.log(mot)
+// console.log(nombre)
+
+// 3. Alert, Promt & Confirm
+// alert("This is an alert!")
+// let visitorName = prompt("What's your name : ")
+// console.log(visitorName)
+// let confirmation = confirm("Are you okay with this ? ")
+// console.log(confirmation)
+
+//4. Operators
+// let firstNumber = 4
+// let secondNumber = 2
+// let addition = firstNumber + secondNumber
+// console.log("Addition is: ", addition)
+// console.log(4 + 2)
+// console.log(4 - 2)
+// console.log(4 * 2)
+// console.log(4 / 2)
+// Modulo: Reste de division
+// console.log(10 % 3)
+//exponentiel
+// console.log(4 ** 2)
+
+//5. Comparaisons
+// let firstNumber = 4
+// let secondNumber = 2
+// let resultTrue = firstNumber >= secondNumber // true
+// let resultFalse = firstNumber <= secondNumber // false
+// console.log(resultTrue)
+// console.log(resultFalse)
+
+// //== egalite des valeurs et === egalite des valeurs + leurs types
+// let firstNumber = 3
+// let secondNumber = "3"
+// let egalite = firstNumber === secondNumber
+// console.log(egalite)
+// // la negation
+// console.log(!egalite)
+
+//6. Operateurs logiques
+// let x = 1 // true
+// let y = 0 // false
+// console.log(x && y) // x et y
+// console.log(x || y) // x ou y
+
+//7. Concatenation des strings
+let firstName = "Ali"
+let lastName = "Alem"
+let fullName = firstName + " " + lastName
+console.log("fullName: ", fullName)
---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Function addition
+function addition(a, b) {
+  let result = a + b
+  return result
+}
+//Function call
+let som1 = addition(4, 3)
+console.log(som1)
+let som2 = addition(5, 6)
+console.log(som2)
+
+//Function Substract
+function substract() {}
+
+//Function Division
+function division() {}
+
+//Function returning fullName
+function fullName() {}
